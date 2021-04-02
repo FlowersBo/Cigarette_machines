762086173cda55e5357c33edcd8d67d74e783c95
FlowersBo <172275766@qq.com>
2021-04-02 17:42:24 +0800

    一阶段完成

--- config/api.js
+++ config/api.js
@@ -1,6 +1,7 @@
-// const ApiRootUrl = 'https://w3.morninggo.cn/'; //晨购
-// const ApiRootUrl = 'https://api.morninggo.cn/';
-const ApiRootUrl = 'http://192.168.126.248:8080/morninggo_app_http_war/';
+// const ApiRootUrl = 'https://w3.morninggo.cn/'; //测试
+const ApiRootUrl = 'https://api.morninggo.cn/';
+// const ApiRootUrl = 'http://192.168.126.248:8080/morninggo_app_http_war/';
+// const ApiRootUrl = 'http://192.168.126.248:8080/app-http/';
 module.exports = {
     RefreshAuth: ApiRootUrl + 'tobaccos/user/refresh_auth', //刷新授权
     Login: ApiRootUrl + 'tobaccos/user/auth', //登录授权
@@ -21,5 +22,5 @@ module.exports = {
     OrderDetails: ApiRootUrl + 'tobaccos/payment/orderDetails',
 
     ProductPrice: ApiRootUrl + 'tobaccos/order/checkedProductPrice', //退款选择后计算机额
-    WxRefund: ApiRootUrl + 'tobaccos/order/wx_refund', //退款选择后计算机额
+    WxRefund: ApiRootUrl + 'tobaccos/order/wx_refund', //退款
 }
